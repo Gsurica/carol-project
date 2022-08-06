ea2654c155e90fe0e9f46fd1b5098e2e0d026210
BuildTools <unconfigured@null.spigotmc.org>
2022-08-06 18:48:03 -0300

    zindex

--- src/shared/components/UIElements/LoveNumber.jsx
+++ src/shared/components/UIElements/LoveNumber.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 export const LoveNumber = () => {
 
-  const love = useSelector(state => state.progress.counter);
+  const love = useSelector(state => state.progress.counter );
+
+  const getLove = () => {
+    return love
+  }
+
+  useEffect(() => {
+    getLove()
+  }, [love])
 
   return (
     <div className="w-full flex items-center justify-center py-4 dark:text-[#A1EF7A] text-[#FFB3C1] bg-zinc-800">
-      <h1 className="text-6xl">{ love }%</h1>
+      <h1 className="text-6xl">{ getLove() }%</h1>
     </div>
   )
 }
