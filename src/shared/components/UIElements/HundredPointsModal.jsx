33ef37096db2e1f53b8ae10af99e0353b6de0969
BuildTools <unconfigured@null.spigotmc.org>
2022-08-06 17:52:24 -0300

    ll

--- src/shared/components/UIElements/HundredPointsModal.jsx
+++ src/shared/components/UIElements/HundredPointsModal.jsx
@@ -10,7 +10,7 @@ export const HundredPointsModal = () => {
   return (
     <>
       <div className="bg-black bg-opacity-90 absolute inset-0 flex items-center w-full justify-evenly text-white" onClick={ () => dispatch(isHundred(false)) }>
-        <video autoPlay loop muted src={ video } type="video/mp4" width="320" height="240" />
+        <video autoPlay loop muted src={ video } type="video/mp4" width="320" height="240" playsInline/>
       </div>
       <div className="w-full flex items-center justify-center mt-8 sm:text-7xl">
         <h1 className="animate-bounce text-white">PARABÉNS FÁBIO!</h1>
--- src/shared/components/UIElements/ThemeSwitcher.jsx
+++ src/shared/components/UIElements/ThemeSwitcher.jsx
@@ -6,14 +6,30 @@ export const ThemeSwitcher = () => {
 
   const { theme, setTheme } = useTheme();
 
+  if (theme === "dark") {
+    return (
+      <>
+        <div className="fixed right-1 bottom-1">
+          <button
+            onClick={ () => setTheme(theme === "light" ? "dark" : "light") }
+          >
+            <div className="p-4 dark:bg-[#DCE9FC] rounded-full flex items-center bg-rose-100 text-3xl text-black z-40">
+              <ion-icon name="football-outline"></ion-icon>
+            </div>
+          </button>
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <div className="fixed right-1 bottom-1">
         <button
           onClick={ () => setTheme(theme === "light" ? "dark" : "light") }
         >
-          <div className="p-4 dark:bg-[#DCE9FC] rounded-full flex items-center bg-rose-100 text-6xl text-white">
-            <ion-icon name="logo-tableau"></ion-icon>
+          <div className="p-4 dark:bg-[#DCE9FC] rounded-full flex items-center bg-rose-100 text-3xl text-black z-40">
+            <ion-icon name="flower-outline"></ion-icon>
           </div>
         </button>
       </div>
